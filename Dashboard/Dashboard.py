be69e43c705db8e780bf2c8fbe93284714826a9b
riantn <riantn0203@gmail.com>
2024-08-11 12:32:47 +0700

    Hapus Babel format currency

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set(style='white')
 
 day_df = pd.read_csv("day.csv")
